7701a32e25f6de285a7f77a542ab8302a96a493b
Mikita <45191288+Raccoon2508@users.noreply.github.com>
2019-10-03 00:11:24 +0300

    zdfdfhnfgjn

--- src/index.js
+++ src/index.js
@@ -1,78 +1,70 @@
-module.exports = function zeros(expression) {
-  let intStr;
-let zeroCount=0;
-let num='';
+module.exports = function function zeros(expression){
+
+
+let intStr=expression;
+let totalCounterTwoFive=[0,0];
+let number='';
 let factFlag=0;
 
-
-
-
-for(let i=0; i<intStr;i++){
+for(let i=0; i<intStr.length;i++){
   if(intStr.charCodeAt(i)>=48&&intStr.charCodeAt(i)<=57){
-    num=num+intStr[i];
-    }
-  else if(intStr[i]=='!'){
+    number=number+intStr[i];
+  
+    }else if(intStr[i]=='!'){
     factFlag++;
     }
   
-  if(instStr[i+1]=="*"||!instStr[i+1]){
-    zeroCount+=getZerosCount(num,factFlag);
+  if(intStr[i+1]=="*"||!intStr[i+1]){
+    
+    let total=getZerosCount(number,factFlag);
+  
+   
+    totalCounterTwoFive[0]+=total[0];
+    totalCounterTwoFive[1]+=total[1];
+    console.log(totalCounterTwoFive)
+    factFlag=0;
+    number='';
     
     }
-  
+}
+
+function getZerosCount(number,factFlag){ 
+
+let prime;
+let counter=[0,0];
+let intakes;
+
+if(number%2==0&&factFlag==2){
+  counter[0]=inputs(number,2);
+    counter[1]=inputs(number,5);
+}else{counter[1]=inputs(number,5);}
+
+
+
+function inputs(number,input){
+    
+    let intakes=1;
+    let counter=0;
+    while(intakes!=0){
+  intakes=Math.floor(number/input);
+  input*=input;
+  counter+=intakes;
   }
+  return counter;
+  }    
 
-
-
-
-
-function getZerosCount(number,flag) {
-  
-  let base=10
-  var primeNumbers;
-  
-  if(flag==2){
-  primeNumbers=[10];
-  }else{primeNumbers=[5];}
-  
-  
-  
-  var degrees=[];
-  var primeFactors=[];
-  var indexOfPrimeFactors=0;
-  for(var i = 0; i<=primeNumbers.length; i++)
-  {
-    var degree=0
-    while((base/primeNumbers[i]^0)===(base/primeNumbers[i]))
-    {
-      base=base/primeNumbers[i];
-      degree++;
-    }
-    if(degree>0)
-    {  
-      primeFactors[indexOfPrimeFactors]=primeNumbers[i];
-      degrees[indexOfPrimeFactors]=degree;
-      indexOfPrimeFactors++;
-    }
-  }
-  var preTotal;
-  var arrOfSumms=[];
-  var totalSum=0;
-  
-  
-  for(i=0; i<degrees.length; i++){
-  preTotal=0;
-  for(j=1; number/Math.pow(primeFactors[i], j)>=1 ; j++){
-    preTotal=preTotal+Math.floor(number/Math.pow(primeFactors[i], j));
-  }
-  totalSum=Math.floor(preTotal/degrees[i])
-  arrOfSumms[i]=totalSum;
-  }
-  
-  var total=Math.min.apply(null, arrOfSumms)
-  return total; 
+return counter;
 }
 
 
 
+if(totalCounterTwoFive[0]<=totalCounterTwoFive[1]){
+  return totalCounterTwoFive[0];
+  }else{
+  return totalCounterTwoFive[1];  
+  }
 }
+
+
+zeros('9!!*10!!*7!!')
+
